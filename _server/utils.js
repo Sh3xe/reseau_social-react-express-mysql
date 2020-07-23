7ef2ad9011d4fd44db8b2f7fd48212d3d5470d6c
Arnaud Pelissier <qwerlyz666@gmail.com>
2020-07-23 10:40:29 +0200

    added default value to generateToken

--- _server/utils.js
+++ _server/utils.js
@@ -1,10 +1,20 @@
-function parseKeyValue(str) {
+"use strict";
+
+function parseKeyValueString(str) {
     let obj = {};
     str.split(";").forEach((e) => {
         const [key, value] = e.split("=");
         obj[key] = value;
     });
     return obj;
+}
+
+function objectToKeyValue(object) {
+    let query = "";
+    for(let [key, value] of Object.entries(object)) {
+        query += `${key}=${value};`;
+    }
+    return query.slice(0, query.length - 1);
 }
 
 function addZeros(value, length, filler = "0") {
@@ -17,11 +27,31 @@ function addZeros(value, length, filler = "0") {
     } catch(e) { return null }
 }
 
-function generateToken(length) {
-    const chars = "0123456789azertyuiopqsdfghjklmwxcvbn";
+function generateToken(length = 255) {
+	const set = "0123456789azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN"
     let token = "";
     for(let i = 0; i < length; i++) {
-        token += chars[Math.round( Math.random() * (chars.length - 1) )];
+		token += set[Math.round( Math.random() * (set.length - 1) )];
     }
     return token;
 }
+
+function formatDate(d) {
+	const {day, month, year, hours, minutes} = {
+		day: addZeros(d.getDate(), 2),
+		month: addZeros(d.getMonth() + 1(), 2),
+		year: d.getFullYear(),
+		hours: addZeros(d.getHours(), 2),
+		minutes: addZeros(d.getMinutes(), 2),
+	}
+	
+	return `${day}/${month}/${year}, ${hours}h${$minutes}`;
+}
+
+module.exports = {
+	parseKeyValueString,
+	objectToKeyValue,
+	addZeros,
+	generateToken,
+	formatDate
+};
